e485124e698b3b35679d01bceeaf0e8bd0c3f896
Andrei-Laurențiu Radu <117320481+andreilaurentiuradu@users.noreply.github.com>
2023-05-16 01:47:55 +0300

    Update checkers.asm
    comentarii

--- checkers.asm
+++ checkers.asm
@@ -1,5 +1,5 @@
 %include "../include/io.mac"
-
+%assign octet 8
 section .data
 
 section .text
@@ -20,12 +20,14 @@ checkers:
     ;; FREESTYLE STARTS HERE
     ; avem 9 cazuri
     ; 4 colturi, 4 rame si centru
+
     mov esi, eax ;linia
     shl esi, 3 ;nr de bytes pt o linie(inmultim cu 2 ^ 3)
     add esi, ebx ;adaugam acum coloana
     add esi, ecx ;pozitia damei
 
 
+    ; verificam daca ne aflam pe una dintre cele 4 rame
     cmp eax, 0
     jz south
     cmp eax, 7
@@ -35,26 +37,30 @@ checkers:
     cmp ebx, 7
     jz east
 
-    ; acum luam vecinii diagonali
-    add dword[esi - 8 - 1], 1
-    add dword[esi - 8 + 1], 1
-    add dword[esi + 8 - 1], 1
-    add dword[esi + 8 + 1], 1
+    ; acum luam vecinii diagonali(nu ne aflam pe rama)
+    add dword[esi - octet - 1], 1
+    add dword[esi - octet + 1], 1
+    add dword[esi + octet - 1], 1
+    add dword[esi + octet + 1], 1
 
-    PRINTF32 `indice:%d\n\x0\n`, eax
     jmp end
 
+; rama de jos
 south:
-    cmp ebx, 0 ; verificam daca e colt 0, 0
+    ; verificam daca e colt
+    cmp ebx, 0
     jz corner_0_0
     cmp ebx, 7
     jz corner_0_7
+    ; daca nu e colt marcam cele 2 pozitii
     add dword[esi + 8 + 1], 1
     add dword[esi + 8 - 1], 1
     jmp end
 
+; rama de sus
 north:
-    cmp ebx, 0 ;
+    ; verificam daca e colt
+    cmp ebx, 0
     jz corner_7_0
     cmp ebx, 7
     jz corner_7_7
@@ -62,31 +68,44 @@ north:
     add dword[esi - 8 - 1], 1
     jmp end
 
+; daca e coltul stanga jos
 corner_0_0:
+    ; marcam pozitia corespunzatoare
     add dword[esi + 8 + 1], 1
     jmp end
 
+; daca e coltul dreapta jos
 corner_0_7:
+    ; marcam pozitia corespunzatoare
     add dword[esi + 8 - 1], 1
     jmp end
 
+; daca e coltul stanga sus
 corner_7_0:
+    ; marcam pozitia corespunzatoare
     add dword[esi - 8 + 1], 1
     jmp end
 
+; daca e coltul dreapta sus
 corner_7_7:
+    ; marcam pozitia corespunzatoare
     add dword[esi - 8 - 1], 1
     jmp end
 
+; rama din stanga(fara colt)
 west:
+    ; marcam cele 2 pozitii
     add dword[esi - 8 + 1], 1
     add dword[esi + 8 + 1], 1
     jmp end
 
+; rama din dreapta(fara colt)
 east:
+    ; marcam cele 2 pozitii
     add dword[esi - 8 - 1], 1
     add dword[esi + 8 - 1], 1
     jmp end
+
 end:
     ;; FREESTYLE ENDS HERE
     ;; DO NOT MODIFY
