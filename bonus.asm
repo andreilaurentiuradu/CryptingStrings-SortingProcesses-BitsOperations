8b2f34bec5357ec1968868f94b1cbcd60369eb27
Andrei-Laurențiu Radu <117320481+andreilaurentiuradu@users.noreply.github.com>
2023-05-15 17:25:09 +0300

    Update bonus.asm
    coloana 0, 4 margini (2 pentru fiecare numar) si 2 cazuri centrale

--- bonus.asm
+++ bonus.asm
@@ -18,254 +18,109 @@ bonus:
     ;; DO NOT MODIFY
     ;; FREESTYLE STARTS HERE
     ; esi e primu edx al doilea
-    ; avem 18 cazuri
-    ; 4 colturi, 4 rame si centru pentru fiecare intreg din edi respectiv edi
     xor esi, esi
-    xor edi, edi
     mov esi, ecx ; partea de sus
+
     add ecx, 4
+    xor edi, edi
     mov edi, ecx ; partea de jos
 
+    ; initializam registrii
+    xor ecx, ecx
+    xor edx, edx
 
+    mov dl, 5 ; punem in dl ca sa putem face shiftarea
+    mov ecx, ebx ; punem coloana in ecx pt shl
+    cmp ecx, 0 ; daca e pe coloana 0
+    jz zero_col
+
+    dec ecx ; ne folosim de ebx pentru a pune 1 pe colturile din stanga
+    shl dl, cl ; pe baza lui ebx(coloana)
+
+
+    jmp zone ; mergem in una din zone
+
+zero_col:
+    ; daca e pe coloana 0
+    shr dl, 1 ;impartim la 2
+
+zone:
+    ; dl contine acum forma byte-ului
+    PRINTF32 `octet:%d\n\x0\n`, edx
+    cmp eax, 4
+    jge up
+    jmp down
+
+down:
+    cmp eax, 0
+    je down_border ; daca e pe marginea de sus
+    cmp eax, 3
+    je down_middle_border ; daca e pe marginea de jos primului nr
+
+center_loop_down:
+    cmp eax, 1
+    je put_bytes_down
+    add esi, 1
+    dec eax
+    jmp center_loop_down
+
+put_bytes_down:
+    mov byte[edi], dl
+    mov byte[edi + 2], dl
+    jmp end
+
+
+up:
+    cmp eax, 7
+    je up_border ; daca e pe marginea de sus
+    cmp eax, 4
+    je up_middle_border ; daca e pe marginea de jos primului nr
+
+; pentru centrul primului nr
+center_loop_up:
+    cmp eax, 5
+    je put_bytes_up
+    add esi, 1
+    dec eax
+    jmp center_loop_up
+
+; punem octetii
+put_bytes_up:
+    mov byte[esi], dl
+    mov byte[esi + 2], dl
+    jmp end
+
+up_border: ; marginea de sus a primului nr
+    mov byte[esi + 2], dl
+    jmp end
+
+up_middle_border: ; marginea de jos a primului nr
+    mov byte[esi + 1], dl
+    PRINTF32 `aici?:%d\n\x0\n`, eax
+    mov byte[edi + 3], dl
+    jmp end
+
+down_middle_border:
+    mov byte[edi + 2], dl
+    mov byte[esi], dl
+    jmp end
+
+down_border:
+    mov byte[edi + 1], dl
+    jmp end
+
+    ; add byte[edi], 128 ; sa adaugam 1 la valoarea din byteul lui esi
     ; PRINTF32 `unu:%d\n\x0\n`, esi
     ; PRINTF32 `doi:%d\n\x0\n`, edx
-    cmp eax, 0
-    jz south
-    cmp eax, 7
-    jz north
-
-    cmp eax, 4
-    jz middle_esi
-
-    cmp eax, 3
-    jz middle_edi
-
-    cmp ebx, 0
-    jz west_high
-    cmp ebx, 7
-    jz east_high
-
-    cmp eax, 4
-    jl center_down
-    ; ai de completat aici cu un for si dupa adaugi la ambele linii
-    ; else center_up
-
-    jmp end
-
-center_down:
-    ; ai de completat aici cu un for si dupa adaugi la ambele linii
-    jmp end
-middle_edi:
-    cmp ebx, 0
-    jz left_edi_corner
-    cmp ebx, 7
-    jz right_edi_corner
-
-    xor edx, edx
-    mov edx, 5
-
-    ;PRINTF32 `linie:%d\x0\n`, eax
-    ;PRINTF32 `coloana:%d\n\x0\n`, ebx
-    ; mutam 5-ul exact cate pozitii avem nevoie la dreapta in functie de ebx care retine coloana
-    ;PRINTF32 `inainte:%d\n\x0\n`, edx
-
-    for_middle_edi:
-        cmp ebx, 1
-        jz continue_middle_edi
-        imul edx, 2
-        ;PRINTF32 `ed:%d\n\x0\n`, edi
-        dec ebx
-        jmp for_middle_edi
-
-    continue_middle_edi:
-    ;PRINTF32 `intra_aici:%d\n\x0\n`, edx
-        add byte[esi], dl
-        add byte[edi + 2], dl
-    jmp end
-
-left_edi_corner:
-    add byte[esi], 2
-    add byte[edi + 2], 2
-    jmp end
-
-right_edi_corner:
-    add byte[esi], 64
-    add byte[edi + 2], 64
-    jmp end
-
-middle_esi:
-
-    cmp ebx, 0
-    jz left_esi_corner
-    cmp ebx, 7
-    jz right_esi_corner
-
-    xor edx, edx
-    mov edx, 5
-
-    ;PRINTF32 `linie:%d\x0\n`, eax
-    ;PRINTF32 `coloana:%d\n\x0\n`, ebx
-    ; mutam 5-ul exact cate pozitii avem nevoie la dreapta in functie de ebx care retine coloana
-    ;PRINTF32 `inainte:%d\n\x0\n`, edx
-    for_middle_esi:
-        cmp ebx, 1
-        jz continue_middle_esi
-        imul edx, 2
-        ;PRINTF32 `mid_esi:%d\n\x0\n`, edx
-        dec ebx
-        jmp for_middle_esi
-
-    continue_middle_esi:
-    ;PRINTF32 `intra_aici:%d\n\x0\n`, edx
-        add byte[esi + 1], dl
-        add byte[edi + 3], dl
-    jmp end
-
-left_esi_corner:
-    add byte[esi + 1], 2
-    add byte[edi + 3], 2
-    jmp end
-
-right_esi_corner:
-    add byte[esi + 1], 64
-    add byte[edi + 3], 64
-    jmp end
-
-south:
-    cmp ebx, 0 ; verificam daca e colt 0, 0 si analog pentru restu
-    jz corner_0_0
-    cmp ebx, 7
-    jz corner_0_7
-
-    ; punem un 5 in edx (echivalentul lui 1 0 1 in binar)
-    ; exact cum trebuie sa fie pe linia anterioara in matrice
-    xor edx, edx
-    mov edx, 5
-
-    ; mutam 5-ul exact cate pozitii avem nevoie la dreapta in functie de ebx care retine coloana
-    for_south:
-        cmp ebx, 1
-        jz continue_south
-        imul edx, 2
-        dec ebx
-        jmp for_south
-
-    continue_south:
-    ; adaugam pe 5 shiftat
-    add byte[edi + 1], dl
-    jmp end
-
-north:
-    cmp ebx, 0
-    jz corner_7_0
-    cmp ebx, 7
-    jz corner_7_7
-
-
-    xor edx, edx
-    mov edx, 5
-    PRINTF32 `da\n\x0\n`, edx
-    ; mutam 5-ul exact cate pozitii avem nevoie la dreapta in functie de ebx care retine coloana
-    for_north:
-        cmp ebx, 1
-        jz continue_north
-        imul edx, 2
-        dec ebx
-        jmp for_north
-
-    continue_north:
-    PRINTF32 `%d\n\x0\n`, edx
-    add byte[esi + 2], dl
-    jmp end
-
-corner_0_0:
-    add byte[edi + 1], 2
-    jmp end
-
-corner_0_7:
-    add byte[edi + 1], 64
-    jmp end
-
-corner_7_0:
-    add byte[esi + 2], 2
-    jmp end
-
-corner_7_7:
-    add byte[esi + 2], 64
-    jmp end
-
-west_high:
-    cmp eax, 3
-    jl west_low
-
-    cmp eax, 6
-    jz next_west_high
-
-    ; 5, 0
-    add byte[esi + 2], 2
-    add byte[esi], 2
-    jmp end
-
-    ; 6, 0
-    next_west_high:
-    add byte[esi + 3], 2
-    add byte[esi + 1], 2
-    jmp end
-
-west_low:
-    PRINTF32 `unu:%d\n\x0\n`, esi
-    cmp eax, 1
-    jz next_west_low
-
-    ; 2, 0
-    add byte[edi + 1], 2
-    add byte[edi + 3], 2
-    jmp end
-
-    ; 1, 0
-
-    next_west_low:
-    add byte[edi], 2
-    add byte[edi + 2], 2
-    jmp end
-
-east_high:
-    cmp eax, 3
-    jl east_low
-
-    cmp eax, 6
-    jz next_east_high
-
-    ; 5, 7
-    add byte[esi + 2], 64
-    add byte[esi], 64
-    jmp end
-
-    ; 6, 7
-    next_east_high:
-    add byte[esi + 3], 64
-    add byte[esi + 1], 64
-    jmp end
-
-east_low:
-    cmp eax, 1
-    jz next_east_low
-
-    ; 2, 7
-    add byte[edi + 1], 64
-    add byte[edi + 3], 64
-    jmp end
-
-    ; 1, 7
-    next_east_low:
-    add byte[edi], 64
-    add byte[edi + 2], 64
-    jmp end
-
+    ;;;;;;;;esi
+    ;;;;;;;;edi
 end:
+
     ;; FREESTYLE ENDS HERE
     ;; DO NOT MODIFY
     popa
     leave
     ret
     ;; DO NOT MODIFY
+
+
