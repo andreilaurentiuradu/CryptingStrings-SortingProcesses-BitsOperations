3aaf32d60e592b074b47f6b2e7f197053a2317b9
Andrei-Laurențiu Radu <117320481+andreilaurentiuradu@users.noreply.github.com>
2023-05-16 02:18:59 +0300

    Update bonus.asm
    comentarii

--- bonus.asm
+++ bonus.asm
@@ -17,76 +17,91 @@ bonus:
 
     ;; DO NOT MODIFY
     ;; FREESTYLE STARTS HERE
-    ; esi e primu edx al doilea
+    ; retinem in esi partea de sus, respectiv in edi partea de jos
     xor esi, esi
-    mov esi, ecx ; partea de sus
-
+    mov esi, ecx
     add ecx, 4
     xor edi, edi
-    mov edi, ecx ; partea de jos
+    mov edi, ecx
 
     ; initializam registrii
     xor ecx, ecx
     xor edx, edx
 
-    mov dl, 5 ; punem in dl ca sa putem face shiftarea
-    mov ecx, ebx ; punem coloana in ecx pt shl
-    cmp ecx, 0 ; daca e pe coloana 0
+    ; punem 00000101 in registrul dl
+    mov dl, 5
+    ; retinem coloana in ecx
+    mov ecx, ebx
+    cmp ecx, 0
     jz zero_col
 
-    dec ecx ; ne folosim de ebx pentru a pune 1 pe colturile din stanga
-    shl dl, cl ; pe baza lui ebx(coloana)
+    ; daca nu e pe coloana 0 cream byteul ce reprezinta linia
+    ; shiftand la stanga in nr cu nr_coloanei - 1 pozitii
+    dec ecx
+    shl dl, cl
 
-
-    jmp zone ; mergem in una din zone
+    jmp zone
 
 zero_col:
-    ; daca e pe coloana 0
-    shr dl, 1 ;impartim la 2
+    ; daca e pe coloana 0 shiftam la dreapta numarul
+    ;(shiftare la stanga a unei linii in matrice)
+    shr dl, 1
 
 zone:
+    ; verificam in ce zona ne aflam in matrice
+    ; (board[0] sau board[1])
     ; dl contine acum forma byte-ului
-    PRINTF32 `octet:%d\n\x0\n`, edx
     cmp eax, 4
     jge up
     jmp down
 
 down:
+    ; daca ne aflam in partea inferioara matricei
+    ; verificam daca se afla pe vreuna din marginile sus/jos
     cmp eax, 0
-    je down_border ; daca e pe marginea de sus
+    je down_border
     cmp eax, 3
-    je down_middle_border ; daca e pe marginea de jos primului nr
+    je down_middle_border
 
 center_loop_down:
+    ; daca nu se afla pe vreo margine
+    ; determinam liniile pe care se poate deplasa dama
     cmp eax, 1
     je put_bytes_down
     add edi, 1
-    ;PRINTF32 `doi:%d\n\x0\n`, edi
     dec eax
     jmp center_loop_down
 
 put_bytes_down:
+    ; punem byteul dl pe liniile corespunzatoare
     mov byte[edi], dl
     mov byte[edi + 2], dl
     jmp end
 
 down_middle_border:
+    ; daca e pe rama de sus a partii inferioare a matricei
+    ; punem byteul dl pe liniile corespunzatoare
     mov byte[edi + 2], dl
     mov byte[esi], dl
     jmp end
 
 down_border:
+    ; daca e pe rama de jos a partii inferioare a matricei
+    ; punem byteul dl pe linia corespunzatoare
     mov byte[edi + 1], dl
     jmp end
 
 up:
+    ; daca ne aflam pe partea superioara a matricei
+    ; verificam daca ne aflam pe vreuna din marginile sus/jos
     cmp eax, 7
-    je up_border ; daca e pe marginea de sus
+    je up_border
     cmp eax, 4
-    je up_middle_border ; daca e pe marginea de jos primului nr
+    je up_middle_border
 
-; pentru centrul primului nr
 center_loop_up:
+    ; daca nu se afla pe vreo margine
+    ; determinam liniile pe care se poate deplasa dama
     cmp eax, 5
     je put_bytes_up
     add esi, 1
@@ -99,22 +114,20 @@ put_bytes_up:
     mov byte[esi + 2], dl
     jmp end
 
-up_border: ; marginea de sus a primului nr
+up_border:
+    ; daca e pe marginea de sus a partii superioare a matricei
+    ; punem byte-ul pe linia corespunzatoare
     mov byte[esi + 2], dl
     jmp end
 
-up_middle_border: ; marginea de jos a primului nr
-    ;PRINTF32 `unu:%d\n\x0\n`, esi
+up_middle_border:
+    ; daca e pe marginea de jos a partii superioare a matricei
+    ; punem byteul pe linia corespunzatoare
     mov byte[esi + 1], dl
     mov byte[edi + 3], dl
 
     jmp end
 
-    ; add byte[edi], 128 ; sa adaugam 1 la valoarea din byteul lui esi
-    ; PRINTF32 `unu:%d\n\x0\n`, esi
-    ; PRINTF32 `doi:%d\n\x0\n`, edx
-    ;;;;;;;;esi
-    ;;;;;;;;edi
 end:
 
     ;; FREESTYLE ENDS HERE
